ce5989f079253b6538d4c92482f346c1d231face
Stepan-DE <stepandwork@outlook.com>
2026-02-15 21:40:58 +0300

    Update data_unpacker.py

--- data_unpacker.py
+++ data_unpacker.py
@@ -4,28 +4,44 @@
 def json_unpacker(game_data):
     result = {}
 
-    result['name']=game_data.get('name')
-    result['appid']=game_data.get('steam_appid')
-    result['min_age']=game_data.get('required_age')
-    result['is_free']=game_data.get('is_free')
-    result['description']=game_data.get('about_the_game')
-    result['supported_languages']=game_data.get('supported_languages')
-    result['header_image']=game_data.get('header_image')
-    result['capsule_image']=game_data.get('capsule_image')
-    result['website']=game_data.get('website')
-    result['pc_requirements']=game_data.get('pc_requirements')
-    result['mac_requirements']=game_data.get('mac_requirements')
-    result['linux_requirements']=game_data.get('linux_requirements')
-    result['developers']=game_data.get('developers')
-    result['publishers']=game_data.get('publishers')
-    result['price']=game_data.get('price_overview')
-    result['platforms']=game_data.get('platforms')
-    result['metacritic']=game_data.get('metacritic')
-    result['categories']=game_data.get('categories')
-    result['genres']=game_data.get('genres')
-    result['screenshots']=game_data.get('screenshots')
-    result['movies']=game_data.get('movies')
-    result['release_date']=game_data.get('release_date')
-    result['movies']=game_data.get('movies')
+    # общая инфомрация
+    result['name'] = game_data.get('name')
+    result['appid'] = game_data.get('steam_appid')
+    result['required_age'] = game_data.get('required_age')
+    result['is_free'] = game_data.get('is_free')
+    result['about_the_game'] = game_data.get('about_the_game')
+    result['supported_languages'] = game_data.get('supported_languages')
+    result['header_image'] = game_data.get('header_image')
+    result['website'] = game_data.get('website')
+    result['developers'] = game_data.get('developers')
+    result['publishers'] = game_data.get('publishers')
 
-    return result # возвращает список
+    # извлекаем цену
+    result['price'] = None
+    package_groups = game_data.get('package_groups', [])
+    if package_groups:
+        subs = package_groups[0].get('subs', [])
+        if subs:
+            result['price'] = subs[0].get('price_in_cents_with_discount')/100
+
+
+    # поддержка платформ
+    result['windows'] = game_data.get('platforms', {}).get('windows')
+    result['mac'] = game_data.get('platforms', {}).get('mac')
+    result['linux'] = game_data.get('platforms', {}).get('linux')
+
+    # котегории и отзывы
+    result['metacritic'] = game_data.get('metacritic', {}).get('score')
+
+    genres_list = game_data.get('genres', [])
+    result['genres_ids'] = [int(g['id']) for g in genres_list if g.get('id') is not None]
+
+    # остальное
+    screenshots = game_data.get('screenshots', [])
+    result['screenshots'] = screenshots[0].get('path_full') if screenshots else None
+
+    result['release_date'] = game_data.get('release_date', {}).get('date')
+    result['support_url'] = game_data.get('support_info', {}).get('url')
+    result['support_email'] = game_data.get('support_info', {}).get('email')
+
+    return result
