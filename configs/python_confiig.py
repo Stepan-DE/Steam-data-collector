c80167f633af147f16d385ab14abdc0c0a9c2ae0
Stepan-DE <stepandwork@outlook.com>
2026-02-15 21:37:07 +0300

    Add files via upload

---
+++ configs/python_confiig.py
@@ -0,0 +1,9 @@
+# имена фалов
+ALL_APPIDS = 'appids/all_appids.txt'
+CORRECT_APPIDS = 'appids/correct_appids.txt'
+FAILED_APPIDS = 'appids/failed_appids.txt'
+WORKING_APPIDS_REFERENCE = 'appids/working_appids(reference).txt'
+WORKING_APPIDS = 'appids/working_appids.txt'
+
+# глобальные переменные
+TOTAL_WIDTH = 120
